a9bb4d2522d7d0d8f776ddc7ec33ac6e4d922818
hafizalihassan202-source <hafizalihassan202@gmail.com>
2026-01-08 00:17:44 +0500

    Constant obj for product

--- first.js
+++ first.js
@@ -1,2 +1,19 @@
+{
 console.log("Hello World");
 console.log("This is the first.js file.");
+fullName = ("John Doe");
+console.log(fullName);
+x = null;
+console.log(x);
+}
+
+// [const obj for product (start) ]
+
+const product = {
+    name: "Laptop",
+    price: 999.99,
+    inStock: true,
+    rattings: 4.5,
+}
+console.log(product);
+// [const obj for product (end) ]
