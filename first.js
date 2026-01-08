d65db2a8cf6036eadf13ef2fc4147aa3a6c67bab
hafizalihassan202-source <hafizalihassan202@gmail.com>
2026-01-09 00:08:33 +0500

    OPerators in JS

--- first.js
+++ first.js
@@ -1,27 +1,84 @@
 
 // [const obj for product (start) ]
 
-const product = {
-    name: "Laptop",
-    price: 999.99,
-    inStock: true,
-    rattings: 4.5,
-}
-console.log(product);
+// const product = {
+//     name: "Laptop",
+//     price: 999.99,
+//     inStock: true,
+//     rattings: 4.5,
+// }
+// console.log(product);
+
 // [const obj for product (end) ]
 
 
 // [const obj for profile (start) ]
-const profile = {
-    name: "Ali HAssan",
-    age: 18,
-    country: "Pakistan",
-    city: "FAisalabad",
-    profession: "Web Developer",
-    followers: 5000,
-}
-console.log(profile.city);
-console.log(profile);
+
+// const profile = {
+//     name: "Ali HAssan",
+//     age: 18,
+//     country: "Pakistan",
+//     city: "FAisalabad",
+//     profession: "Web Developer",
+//     followers: 5000,
+// }
+// console.log(profile.city);
+// console.log(profile);
+
 // [const obj for profile (end) ]
 
+//  Arthematic Operators
 
+// {
+// let a = 10;
+// let b = 5;
+// console.log("a+b =", a+b);
+// console.log("a-b =", a-b);
+// console.log("a*b =", a*b);
+// console.log("a/b =", a/b);
+// console.log("a%b =", a%b);
+// console.log("a^b =", a**b);
+// console.log("++a =", ++a);
+// console.log("--b =", --b);
+// console.log("a++ =", a++);
+// console.log(a);
+// }
+
+// Short HAnd Assignment Operartors
+
+// {
+// let x = 10;
+// let y = 8;
+// console.log("x+y =", x+=y);
+// console.log("x-y =", x-=y);
+// console.log("x*y =", x*=y);
+//  console.log("x/y =", x/=y);
+//  console.log("x%=y =", x%=y);
+// }
+
+// Comparison Operators
+
+// {
+//     let a = 0.3;
+//     let b = "0.3";
+//     console.log("a==b :", a==b);
+//     console.log("a!=b :", a!=b);
+//     console.log("a===b :", a===b);
+//     console.log("(a===b) :", !(a===b) );
+//     console.log("a>b :", a>b);
+//     console.log("a<b :", a<b);
+//     console.log("a>=b :", a>=b);
+//     console.log("a<=b :", a<=b);
+//     console.log("a!>=b :", !(a>=b));
+
+// }
+
+// Logical Operators"
+
+{
+    cond1 = 0.3<0.5;
+    cond2 = 0.5===0.5;
+    console.log("cond1 && cond2 :", cond1 && cond2);
+    console.log("cond1 || cond2 :", cond1 || cond2);
+    console.log("!cond1 :", !cond1);
+}
